d2d39295edea737bd965e6a9390851666e5a0f1c
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-08-21 21:58:22 +0530

    add ssh key file in ssh command output

--- out.tf
+++ out.tf
@@ -1,14 +1,12 @@
 output "ssh_command_master_node" {
-  value       = <<-SSHCOMMAND
-  ssh ubuntu@${aws_instance.master.public_dns}
-  SSHCOMMAND
+  value       = "ssh -i ~/.ssh/${aws_key_pair.this.key_name}.pem ubuntu@${aws_instance.master.public_dns}"
   description = "ssh command for connecting to the master node"
 }
 
 output "ssh_command_agent_nodes" {
   value       = <<-SSHCOMMAND
   %{for dns in aws_instance.agents[*].public_dns}
-  ssh ubuntu@${dns}
+  ssh -i ~/.ssh/${aws_key_pair.this.key_name}.pem ubuntu@${dns}
   %{endfor}
   SSHCOMMAND
   description = "ssh command for connect to the agent nodes"
