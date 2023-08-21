58343d118105a54cf1103f532bfb7cdd9d2ccce5
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-08-21 21:42:33 +0530

    create ssh key pair

--- main.tf
+++ main.tf
@@ -44,10 +44,25 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-resource "aws_key_pair" "webserver" {
-  key_name   = "webserver"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMK0sHujc2rfg3y4blCK46ye7jMTmDa3jXqGVL68ts87zo95JZdQakLQlLiUkVvm7IYOePctjUZDRtaUgnWPWdC/QYQvLLiAkO/IzgcHIyvURNjpVd94I67afYygzIeagsKQJW+igVeztw95fkwW8gvb2DirM4UkOEN6FnGMjBDOFjTJSu1AKqgzr7cqV7JMdVqDLlODGS6Q++H/xv2WTFNXNBR9NFTtTrxJl5+iFKrtg8D/T+1/5LvUwclDd9un9Jb3SYCtxwRK00I3E1nNSviT1zxsUrtkSMt8B+qngwUGjT5whIdpV3109qFt9F4/lEbcrpTtuoiH/nCgQYNL7CZMKVn12r0jg/ADOHpqj56sxh81Vx/NWRi4t1H8d7OZ/aQ3pFMtW1fB+kcdvwT1fNJRAW1U1m6rR2yd6QjUR+5jiBLfvpH6PIwdn1OpVPGRtAUIQxkOeRkTeUsAbvBm2PvWSnoAtgLv/8GzlzY00uw0odLlj/RhJlIHYkyXqETTs= sagarmaurya@sagar.local"
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
+
+resource "aws_key_pair" "this" {
+  key_name   = "puppet"
+  public_key = tls_private_key.this.public_key_openssh
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.this.private_key_openssh}' > ~/.ssh/${self.key_name}.pem; chmod 400 ~/.ssh/${self.key_name}.pem"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -rf ~/.ssh/${self.key_name}.pem"
+  }
+}
+
 
 resource "aws_instance" "master" {
   ami           = var.image_id
@@ -59,7 +74,7 @@ resource "aws_instance" "master" {
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   subnet_id              = aws_subnet.public.id
-  key_name               = aws_key_pair.webserver.key_name
+  key_name               = aws_key_pair.this.key_name
 }
 
 resource "aws_instance" "agents" {
@@ -73,5 +88,5 @@ resource "aws_instance" "agents" {
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   subnet_id              = aws_subnet.public.id
-  key_name               = aws_key_pair.webserver.key_name
+  key_name               = aws_key_pair.this.key_name
 }
